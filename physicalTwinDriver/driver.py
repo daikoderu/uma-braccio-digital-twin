d77b545145d0e4170e0b7b9e1101facc00854af1
DanielSDVG <daniperezporras@gmail.com>
2022-04-30 16:43:36 +0200

    now driver shows all serial port output

--- physicalTwinDriver/driver.py
+++ physicalTwinDriver/driver.py
@@ -1,3 +1,6 @@
+import sys
+from threading import Thread
+
 import redis
 import serial
 import time
@@ -9,7 +12,9 @@ class Braccio:
 
     def write(self, string):
         self.port.write(string.encode())
-        print(self.port.readline())
+
+    def read(self):
+        return self.port.readline().decode().strip()
 
 def enter_redis_hostport():
     host, _, port_str = input("Enter Data Lake host and port: ").partition(":")
@@ -23,14 +28,62 @@ def enter_redis_hostport():
             pass
     return host, port
 
-if __name__ == "__main__":
-    # Create data lake connection
-    # host, port = enter_redis_hostport()
-    # datalake = redis.Redis(host, port)
 
-    # Read serial port
-    serialport = input("Enter serial port: ")
-    robot = Braccio(serialport)
-
+def input_channels(robot, dl):
     while True:
+        # Here we would check for commands from the Data Lake
+        # for now, we just show a prompt
         robot.write(input("> "))
+
+
+def output_channels(robot, dl):
+    while True:
+        out = robot.read()
+        if out:
+            print(f"[RECEIVED: {out}]")
+
+
+def setup():
+    try:
+        # Create data lake connection
+        # host, port = enter_redis_hostport()
+        # dl = redis.Redis(host, port)
+
+        # Read serial port
+        serialport = input("Enter serial port: ")
+        robot = Braccio(serialport)
+
+        return {
+            "success": True,
+            "robot": robot,
+            "dl": None,
+        }
+    except serial.serialutil.SerialException as ex:
+        return {
+            "success": False,
+            "error": f"Serial port error: {ex}"
+        }
+    except KeyboardInterrupt as ex:
+        return {
+            "success": False,
+            "error": "Goodbye!"
+        }
+
+def main():
+    setup_result = setup()
+    if (setup_result["success"]):
+        robot, dl = setup_result["robot"], setup_result["dl"]
+        input_thread = Thread(target=input_channels, name="InputThread", args=(robot, dl))
+        output_thread = Thread(target=output_channels, name="OutputThread", args=(robot, dl))
+
+        input_thread.start()
+        output_thread.start()
+
+        input_thread.join()
+        output_thread.join()
+    else:
+        print(setup_result["error"])
+        return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
